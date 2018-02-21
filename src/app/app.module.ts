9f5a7b5d5135a73407751f3e41169e89d989f2d5
haripoorna <haripoornakumar@gmail.com>
2018-02-21 11:46:26 +0530

    list animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AnimationsComponent } from './animations/animations.component';
 // import { AnimateComponent } from './animate/animate.component';
+import { HeaderComponent} from './header/header.component';
+import { ListAnimationComponent} from './list-animation/list-animation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimationsComponent,
+    HeaderComponent,
+    ListAnimationComponent
     // AnimateComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: AnimationsComponent },
+      { path: 'boxanimation', component: AnimationsComponent },
+      { path: 'listanimation', component: ListAnimationComponent },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
